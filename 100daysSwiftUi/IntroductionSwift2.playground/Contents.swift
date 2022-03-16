73af916d808732a4e913a56486453ca79002d78d
André Silva <2493377+askpt@users.noreply.github.com>
2022-03-16 21:44:02 +0000

    Completed day 3

---
+++ 100daysSwiftUi/IntroductionSwift2.playground/Contents.swift
@@ -0,0 +1,120 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+// Arrays
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
+
+beatles.append("Adrian")
+beatles.append("Allen")
+
+var scores = Array<Int>()
+scores.append(100)
+scores.append(80)
+scores.append(85)
+print(scores[1])
+
+var albums = [String]()
+albums.append("Folklore")
+albums.append("Fearless")
+albums.append("Red")
+
+var albums2 = ["Folklore"]
+albums2.append("Fearless")
+albums2.append("Red")
+print(albums.count)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+
+characters.remove(at: 2)
+print(characters.count)
+
+characters.removeAll()
+print(characters.count)
+
+let bondMovies = ["Casino Royale", "Spectre",  "No Time to Die"]
+print(bondMovies.contains("Frozen"))
+
+let cities = ["London", "Tokyo", "Rome", "Budapest"]
+print(cities.sorted())
+
+let presidents = ["Bush", "Obama", "Trump", "Biden"]
+let resersedPresidents = presidents.reversed()
+print(resersedPresidents)
+
+// Dictionary
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+
+let employee2 = ["name": "Taylor Swift", "job": "Singer", "location": "Nashville"]
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false,
+]
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+
+print(olympics[2012, default: "Unknown"])
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+
+archEnemies["Batman"] = "Penguin"
+
+// Data lookup
+let actors = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+print(actors)
+
+var people = Set<String>()
+people.insert("Denzel Washington")
+people.insert("Tom Cruise")
+people.insert("Nicolas Cage")
+people.insert("Samuel L Jackson")
+
+// Enums
+var selected = "Monday"
+selected = "Tuesday"
+
+enum Weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+
+var day = Weekday.monday
+day = Weekday.tuesday
+
+enum Weekday2 {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day2 = Weekday2.monday
+day2 = .tuesday
+day2 = .friday
--- 100daysSwiftUi/README.md
+++ 100daysSwiftUi/README.md
@@ -7,4 +7,4 @@
 
 ## Status
 
-Currently: 2/100
+Currently: 3/100
