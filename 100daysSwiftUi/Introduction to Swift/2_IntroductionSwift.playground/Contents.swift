c8ab556208d7dca1daf48e28fcbd0d7739e51c2c
André Silva <2493377+askpt@users.noreply.github.com>
2022-03-17 12:11:41 +0000

    Reorganised folder structure

--- 100daysSwiftUi/Introduction to Swift/2_IntroductionSwift.playground/Contents.swift
+++ 100daysSwiftUi/Introduction to Swift/2_IntroductionSwift.playground/Contents.swift
@@ -118,3 +118,40 @@ enum Weekday2 {
 var day2 = Weekday2.monday
 day2 = .tuesday
 day2 = .friday
+
+// Type annotations
+let surname: String = "Lasso"
+var score: Double = 0
+
+let playerName: String = "Roy"
+var luckyNumber: Int = 13
+let pi: Double = 3.141
+var isAuthenticated: Bool = true
+
+var albums1: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
+
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+var teams: [String] = [String]()
+var cities1: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style: UIStyle = UIStyle.light
+style = .dark
+
+let username: String
+// Lots of complex logic
+username = "@towstraws"
+// Lots more complex logic
+print(username)
+
+// Checkpoint 2
+var arr = [ "Popcorn 2022", "Test", "Test", "abc" ]
+print(arr.count)
+var unique = Set(arr)
+print(unique.count)
